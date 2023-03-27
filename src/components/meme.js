f2972d7514c893a21d906a1694b5a276e3ae81f9
nergız seyidova <nergisseyidova@gmail.com>
2023-03-28 02:23:50 +0400

    first

--- src/components/meme.js
+++ src/components/meme.js
@@ -2,9 +2,21 @@ import React from 'react';
 import data from './../data.js';
 
 export default function Meme() {
+  let [meme, setFunc] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImg: '',
+  });
+
+  let [allMemeimg] = React.useState(data);
+
   function getMemeImg() {
-    const memes = data.data.memes;
+    const memes = allMemeimg.data.memes;
     const randomNum = Math.floor(Math.random() * memes.length);
+    setFunc({
+      ...meme,
+      randomImg: memes[randomNum].url,
+    });
   }
 
   return (
@@ -24,6 +36,9 @@ export default function Meme() {
       <button className="btn" onClick={getMemeImg}>
         Get a new meme image 🖼
       </button>
+      <div className="memeImg_container">
+        <img src={meme.randomImg} alt="meme" className="meme_img"></img>
+      </div>
     </main>
   );
 }
